794bf767c9d8c4be1b54bacb5ff1c67842b1f41f
Arash Hatami <hatamiarash7@gmail.com>
2023-03-18 22:46:39 +0330

    Add PrettyPrint func

--- pkg/utils.go
+++ pkg/utils.go
@@ -1,6 +1,8 @@
 package arvancloud
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/url"
 
 	"github.com/google/go-querystring/query"
@@ -9,4 +11,16 @@ import (
 func buildURI(path string, options interface{}) string {
 	v, _ := query.Values(options)
 	return (&url.URL{Path: path, RawQuery: v.Encode()}).String()
+}
+
+// PrettyPrint will print the contents of the object
+func PrettyPrint(data interface{}) {
+	p, err := json.MarshalIndent(data, "", "\t")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("%s \n", p)
 }
