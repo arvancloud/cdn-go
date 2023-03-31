2189cf169f31fe35f276faebe0b369975e38c7ba
Arash Hatami <hatamiarash7@gmail.com>
2023-03-31 19:04:56 +0330

    test: create TestGetDNSRecord func

--- pkg/dns_test.go
+++ pkg/dns_test.go
@@ -112,3 +112,90 @@ func TestCreateDNSRecord(t *testing.T) {
 
 	assert.Equal(t, want, actual)
 }
+
+func TestGetDNSRecord(t *testing.T) {
+	setup()
+	defer teardown()
+
+	recordID := "714009ff-a43c-43c5-80e2-0b3ffc1344a4"
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method, "Expected method 'GET', got %s", r.Method)
+
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, `{
+			"data": {
+				"id": "714009ff-a43c-43c5-80e2-0b3ffc1344a4",
+				"type": "a",
+				"name": "@",
+				"value": [
+					{
+						"ip": "1.2.3.4",
+						"port": 1,
+						"weight": 10,
+						"country": ""
+					},
+					{
+						"ip": "5.6.7.8",
+						"port": 2,
+						"weight": 20,
+						"country": ""
+					}
+				],
+				"ttl": 120,
+				"cloud": false,
+				"upstream_https": "https",
+				"ip_filter_mode": {
+					"count": "multi",
+					"order": "weighted",
+					"geo_filter": "none"
+				},
+				"created_at": "2023-03-31T08:57:51+00:00",
+				"updated_at": "2023-03-31T08:57:51+00:00"
+			}
+		}`)
+	}
+
+	mux.HandleFunc("/domains/"+testDomain+"/dns-records/"+recordID, handler)
+
+	want := &DNSRecord{
+		ID:   recordID,
+		Type: "a",
+		Name: "@",
+		Value: []interface{}{
+			map[string]interface{}{
+				"ip":      "1.2.3.4",
+				"port":    1.0,
+				"weight":  10.0,
+				"country": "",
+			},
+			map[string]interface{}{
+				"ip":      "5.6.7.8",
+				"port":    2.0,
+				"weight":  20.0,
+				"country": "",
+			},
+		},
+		TTL:           120,
+		Cloud:         false,
+		UpstreamHTTPS: "https",
+		IPFilterMode: map[string]interface{}{
+			"count":      "multi",
+			"order":      "weighted",
+			"geo_filter": "none",
+		},
+		CreatedAt: "2023-03-31T08:57:51+00:00",
+		UpdatedAt: "2023-03-31T08:57:51+00:00",
+	}
+
+	_, err := client.GetDNSRecord(context.Background(), ResourceDomain(""), recordID)
+	assert.ErrorIs(t, err, ErrMissingDomain)
+
+	_, err = client.GetDNSRecord(context.Background(), ResourceDomain(testDomain), "")
+	assert.ErrorIs(t, err, ErrMissingDNSRecordID)
+
+	actual, err := client.GetDNSRecord(context.Background(), ResourceDomain(testDomain), recordID)
+	require.NoError(t, err)
+
+	assert.Equal(t, want, actual)
+}
