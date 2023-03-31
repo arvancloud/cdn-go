494a896445985e7d9df780693e5078ec2185e9f8
Arash Hatami <hatamiarash7@gmail.com>
2023-03-31 20:11:56 +0330

    test: create TestHeaders func

--- pkg/arvancloud_test.go
+++ pkg/arvancloud_test.go
@@ -3,6 +3,9 @@ package arvancloud
 import (
 	"net/http"
 	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 var (
@@ -31,4 +34,40 @@ func setup(opts ...Option) {
 
 func teardown() {
 	server.Close()
+}
+
+func TestHeaders(t *testing.T) {
+	// Default
+	setup()
+	mux.HandleFunc("/domains/"+testDomain+"/dns-records", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method, "Expected method 'GET', got %s", r.Method)
+		assert.Equal(t, "application/json", r.Header.Get("Content-Type"))
+	})
+	teardown()
+
+	// Override defaults
+	headers := make(http.Header)
+	headers.Set("Content-Type", "application/graphql")
+	headers.Add("H1", "V1")
+	headers.Add("H2", "V2")
+	setup(Headers(headers))
+	mux.HandleFunc("/domains/"+testDomain+"/dns-records", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method, "Expected method 'GET', got %s", r.Method)
+		assert.Equal(t, "application/graphql", r.Header.Get("Content-Type"))
+		assert.Equal(t, "V1", r.Header.Get("H1"))
+		assert.Equal(t, "V2", r.Header.Get("H2"))
+	})
+	teardown()
+
+	// Authentication
+	setup()
+	client, err := New("TEST_TOKEN")
+	assert.NoError(t, err)
+	client.BaseURL = server.URL
+	mux.HandleFunc("/domains/"+testDomain+"/dns-records", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method, "Expected method 'GET', got %s", r.Method)
+		assert.Equal(t, "Bearer TEST_TOKEN", r.Header.Get("Authorization"))
+		assert.Equal(t, "application/json", r.Header.Get("Content-Type"))
+	})
+	teardown()
 }
